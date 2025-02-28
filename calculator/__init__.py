74abb9a4b728c38e6d3b3e83858f89b100d5b476
VladislavZyuzin <VLAD_z01@inbox.ru>
2025-02-28 13:53:52 +0300

    Delete calculator directory

--- calculator/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Пакет calculator даёт функции для выполнения базовых мат. операций.
-"""
-
-from .calculator import add, subtract, multiply, divide
-
-__all__ = ['add', 'subtract', 'multiply', 'divide']
